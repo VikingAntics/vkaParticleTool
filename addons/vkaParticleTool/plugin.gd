2b15ae48bcbc87943ab38966c4549f7fc37e5f63
VikingAntics <hnorse@esroninc.com>
2024-03-03 22:33:56 -0500

    Minor Fix

--- addons/vkaParticleTool/plugin.gd
+++ addons/vkaParticleTool/plugin.gd
@@ -6,16 +6,14 @@ var particleControlPanel: Control = preload("res://addons/vkaParticleTool/partic
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	#add_control_to_bottom_panel(particleControlPanel, "Particle Control Panel")
-	#AparticleControlPanel.editor_interface = get_editor_interface()
-	particleControlPanel.set_editor_interface(get_editor_interface())
+	if particleControlPanel.has_method("set_editor_interface"):
+		particleControlPanel.set_editor_interface(get_editor_interface())
 	add_control_to_container(EditorPlugin.CONTAINER_INSPECTOR_BOTTOM, particleControlPanel)
 	particleControlPanel.visible = false
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	#remove_control_from_bottom_panel(particleControlPanel)
 	remove_control_from_container(EditorPlugin.CONTAINER_INSPECTOR_BOTTOM, particleControlPanel)
 	particleControlPanel.queue_free()
 
